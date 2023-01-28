367fe5b6f4a16377da47130e01df13fb2b21c23d
KaustubhKamde <123807623+KaustubhKamde@users.noreply.github.com>
2023-01-28 23:53:00 +0530

    Update 1) Annotation through Mouse-Click.py

--- Task 1/1) Annotation through Mouse-Click.py
+++ Task 1/1) Annotation through Mouse-Click.py
@@ -1,15 +1,11 @@
 import cv2
 
-# Load the image
 img = cv2.imread(r'C:\Users\ASUS\Desktop\Tarsyer\Task_1.jpg')
 
-# Allow the user to select a rectangular region of the image
 r = cv2.selectROI(img)
 
-# Crop the image to the selected region
 cropped_img = img[int(r[1]):int(r[1]+r[3]), int(r[0]):int(r[0]+r[2])]
 
-# Save the cropped image
 cv2.imwrite("Task_1_cropped.jpg", cropped_img)
 
 # Draw a rectangle on the original image to indicate the selected region
@@ -22,5 +18,4 @@ cv2.putText(img, f'Bottom Right: ({r[0]+r[2]}, {r[1]+r[3]})', (int(r[0]), int(r[
 # Save the image with the rectangle drawn on it
 cv2.imwrite("Task_1_insights.jpg", img)
 
-# Close the window
 cv2.destroyAllWindows()
